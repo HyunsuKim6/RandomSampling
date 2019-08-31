8bf9b220b8c6248ed4d7b451389d280bef6b4056
Hyunsu Kim <loonland@naver.com>
2019-08-31 19:38:47 +0900

    Create VideoCapture.py

---
+++ VideoCapture.py
@@ -0,0 +1,49 @@
+import cv2
+import os
+
+# Playing video from file:
+cap = cv2.VideoCapture()
+
+try:
+    if not os.path.exists('data'):
+        os.makedirs('data')
+except OSError:
+    print ('Error: Creating directory of data')
+
+currentFrame = 0
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read("")
+
+    if not ret: break
+
+    # Saves image of the current frame in jpg file
+    name = './data/frame' + str(currentFrame) + '.jpg'
+    print ('Creating...' + name)
+    cv2.imwrite(name, frame)
+
+    # To stop duplicate images
+    currentFrame += 1
+
+dirname = r""
+filenames = os.listdir(dirname)
+
+try:
+    if not os.path.exists('Result'):
+        os.makedirs('Result')
+except OSError:
+    print ('Error: Creating directory of data')
+
+for filename in filenames:
+    img = cv2.imread(dirname + "\\" + filename)
+    x = 448; y = 88;
+    w = 1024; h = 1024;
+    img_tilled = img[int(y): int(y + h), int(x): int(x + w)]
+    path = r""
+    name = './Result' + "/" + filename
+    cv2.imwrite(path + "\\" + filename, img_tilled)
+    print("Tiling..." + name)
+
+cap.release()
+cv2.destroyAllWindows()
